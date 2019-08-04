b386ee513bf0d772457b5250ed91db1840d53814
David Fong <vkelim@bigpond.com>
2019-08-04 14:51:18 +1000

    explicit call to library(DailyMeasure)
    required for icon to be found

--- app/app.R
+++ app/app.R
@@ -2,6 +2,5 @@
 # note that 'DailyMeasure' does actually need to be
 # loaded with library() beforehand for the icons to
 # be found in the path!
-# loading DailyMeasure is done in an earlier section
-# as a 'required' library
+library(DailyMeasure)
 DailyMeasure::DailyMeasure()
